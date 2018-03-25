a3021e4fda61fba3272730f2e66ef5ed07107577
Kevin <kevnaf@gmail.com>
2018-03-24 17:50:53 -0700

    pass two

--- Lab04/Lab04/GameBoard.cs
+++ Lab04/Lab04/GameBoard.cs
@@ -4,17 +4,18 @@ using System.Text;
 
 namespace Lab04
 {
-    class GameBoard
+    public class GameBoard
     {
         //Why static?
-        private static string[,] gameBoard = new string[3, 3] { { "|@|", "|@|", "|@|" }, { "|@|", "|@|", "|@|" }, { "|@|", "|@|", "|@|" } };
+        private static string[,] gameBoard = new string[3, 3] { { "|0|", "|1|", "|2|" }, { "|3|", "|4|", "|5|" }, { "|6|", "|7|", "|8|" } };
+        private int[,] locTrans = new int[9, 2] { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 }, { 2, 2 } };
 
         public string[,] Board { get; set; } = gameBoard;
 
-        public string[,] UpdateBoard(int location)
+        public string[,] UpdateBoard(int location, string symbol)
         {
-            string[,] newBoard = new string[3, 3];
-
+            string[,] newBoard = gameBoard;
+            newBoard[locTrans[location, 0], locTrans[location, 1]] = $"|{symbol}|";
             return newBoard;
         }
 
@@ -26,6 +27,51 @@ namespace Lab04
             sb.Append($"{gameBoard[2, 0]} {gameBoard[2, 1]} {gameBoard[2, 2]}\n");
 
             return sb.ToString();
+        }
+
+        public bool CheckWinner(string[,] gameBoard)
+        {
+            //Checks from 00
+            if (gameBoard[0,0] == gameBoard[0, 1] && gameBoard[0, 0] == gameBoard[0, 2]) //Top horizontal
+            {
+                return true;
+            }
+            else if (gameBoard[0, 0] == gameBoard[1, 0] && gameBoard[0, 0] == gameBoard[2, 0]) //Left vertical
+            {
+                return true;
+            }
+            else if (gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[0, 0] == gameBoard[2, 2]) //Backslash diagonal
+            {
+                return true;
+            }
+            //Checks from 01
+            else if (gameBoard[0, 1] == gameBoard[1, 1] && gameBoard[0, 1] == gameBoard[2, 1]) //Middle vertical
+            {
+                return true;
+            }
+            //Checks from 02
+            else if (gameBoard[0, 2] == gameBoard[1, 2] && gameBoard[0, 2] == gameBoard[2, 2]) //Right vertical
+            {
+                return true;
+            }
+            else if (gameBoard[0, 2] == gameBoard[1, 1] && gameBoard[0, 2] == gameBoard[2, 0]) //Forwardslash diagonal
+            {
+                return true;
+            }
+            //Checks from 10
+            else if (gameBoard[1, 0] == gameBoard[1, 1] && gameBoard[1, 0] == gameBoard[1, 2]) //Middle horizontal
+            {
+                return true;
+            }
+            //Checks from 20
+            else if (gameBoard[2, 0] == gameBoard[2, 1] && gameBoard[2, 0] == gameBoard[2, 2]) //Bottom horizontal
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Lab04
 {
@@ -6,9 +7,118 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            MenuLoop();
+        }
+
+        public static void MenuLoop()
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.WriteLine("Would you like to play a game?\ny or n?");
+                string startGame = Console.ReadLine().ToLower();
+                if (startGame == "y")
+                {
+                    Console.WriteLine("Welcome to Tic-Tac-Toe\nLets get our players!");
+                    Player playerOne = (PlayerSetup("One"));
+                    Player playerTwo = (PlayerSetup("Two"));
+
+                    Console.WriteLine($"Player one is {playerOne.Name} Using {playerOne.Symbol}");
+                    Console.WriteLine($"Player two is {playerTwo.Name} Using {playerTwo.Symbol}");
+
+                    Console.ReadLine();
+
+                    GameLoop(playerOne, playerTwo);
+                }
+                else if (startGame == "n")
+                {
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry that was not one of your choices.");
+                }
+            }
+        }
+
+        public static Player PlayerSetup(string playerNum)
+        {
+            try
+            {
+                Console.WriteLine($"Player {playerNum} please state your name.");
+                string playerName = Console.ReadLine();
+                string playerSymbol = "X";
+                while (true)
+                {
+                    Console.WriteLine($"Player {playerNum} please chose a non-numeric symbol to represetn your markers.");
+                    playerSymbol = Console.ReadLine();
+                    if (playerSymbol.Length > 1 && !(Regex.IsMatch(playerSymbol, @"[0-9]")))
+                    {
+                        Console.WriteLine("I'm sorry that is not a valid response");
+                        Console.WriteLine(); //Console formating
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return new Player(playerName, playerSymbol);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void GameLoop(Player playerOne, Player playerTwo)
+        {
             GameBoard gameBoard = new GameBoard();
-            Console.WriteLine(gameBoard.StringBoard(gameBoard.Board));
-            Console.ReadLine();
+            Console.WriteLine("Lets play!");
+            bool winCheck = true;
+            string winner = "";
+            while (winCheck)
+            {
+                MakeMove(playerOne, gameBoard);
+                if (gameBoard.CheckWinner(gameBoard.Board))
+                {
+                    winCheck = false;
+                    winner = playerOne.Name;
+                }
+                MakeMove(playerTwo, gameBoard);
+                if (gameBoard.CheckWinner(gameBoard.Board))
+                {
+                    winCheck = false;
+                    winner = playerTwo.Name;
+                }
+            }
+            Console.WriteLine($"The winner is {winner}\n\n1) Play again\n2) Return to menu");
+
+        }
+
+        public static void MakeMove(Player player, GameBoard gameBoard)
+        {
+            try
+            {
+                Console.WriteLine(); //Console formating
+                Console.WriteLine(gameBoard.StringBoard(gameBoard.Board));
+                Console.WriteLine(); //Console formating
+                Console.WriteLine($"{player.Name} please make your move by choosing a space. Enter 9 to go back to the menu.");
+                int target = int.Parse(Console.ReadLine());
+                if (target == 9)
+                {
+                    MenuLoop();
+                }
+                else
+                {
+                    gameBoard.UpdateBoard(target, player.Symbol);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
